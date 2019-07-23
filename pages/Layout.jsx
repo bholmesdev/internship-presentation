4b12baa6beb0739924d9e1b92607e1eb3be35388
bholmesdev <benjamin.holmes@onepeloton.com>
2019-07-23 14:10:00 -0400

    finish first draft

---
+++ pages/Layout.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { Global, css } from "@emotion/core";
+import PelotonLogo from "./PelotonLogo";
+
+export default ({ children }) => (
+  <>
+    <Global
+      styles={css`
+        h1,
+        h2,
+        h3 {
+          letter-spacing: 0.1em;
+          text-align: center;
+          text-transform: uppercase;
+        }
+        p {
+          color: #ddd;
+          text-align: center;
+        }
+
+        a {
+          color: rgb(255, 51, 71);
+          text-decoration-style: dotted;
+        }
+        img {
+          margin: auto;
+          max-height: 70vh;
+        }
+      `}
+    />
+    <section
+      css={css`
+        display: flex;
+        padding: 20px 60px;
+        padding-bottom: 60px;
+        flex-direction: column;
+        justify-content: center;
+      `}
+    >
+      {children}
+      <div
+        css={css`
+          position: absolute;
+          bottom: 0;
+          left: 0;
+          right: 0;
+          padding: 10px 20px;
+          background: linear-gradient(20deg, black, #333);
+          border-top: 1px solid rgb(255, 51, 71);
+          display: flex;
+          justify-content: flex-end;
+        `}
+      >
+        <PelotonLogo />
+      </div>
+    </section>
+  </>
+);
---
+++ pages/theme.js
@@ -0,0 +1,12 @@
+export default {
+  fonts: {
+    heading: "brandon-grotesque, sans-serif",
+    body: "roboto, sans-serif",
+    monospace: '"Roboto Mono", monospace'
+  },
+  colors: {
+    text: "white",
+    background: "black",
+    primary: "red"
+  }
+};
