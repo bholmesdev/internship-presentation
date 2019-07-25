989ea0bfa5eef409c4911d9ea8fb01ea77e2074f
bholmesdev <benjamin.holmes@onepeloton.com>
2019-07-25 08:46:02 -0400

    add new layout components

--- pages/Layout.jsx
+++ pages/Layout.jsx
@@ -15,8 +15,13 @@ export default ({ children }) => (
         }
         p {
           color: #ddd;
-          text-align: center;
           margin: 0;
+          text-align: left;
+          margin-bottom: 30px;
+        }
+
+        small {
+          font-size: 0.6em;
         }
 
         a {
@@ -25,7 +30,18 @@ export default ({ children }) => (
         }
         img {
           margin: auto;
+          margin-top: 30px;
           max-height: 70vh;
+          max-width: 50vw;
+        }
+        mark {
+          background: rgb(255, 51, 71);
+          color: white;
+        }
+        li {
+          margin-left: 20px;
+          line-height: 1.5em;
+          text-align: left;
         }
       `}
     />
@@ -46,8 +62,8 @@ export default ({ children }) => (
           left: 0;
           right: 0;
           padding: 10px 20px;
-          background: linear-gradient(20deg, black, #333);
-          border-top: 1px solid rgb(255, 51, 71);
+          /* background: linear-gradient(45deg, black, #222); */
+          /* border-top: 1px solid rgb(255, 51, 71); */
           display: flex;
           justify-content: flex-end;
         `}
@@ -56,4 +72,46 @@ export default ({ children }) => (
       </div>
     </section>
   </>
+);
+
+export const Center = ({ children }) => (
+  <div
+    css={css`
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      text-align: center;
+    `}
+  >
+    {children}
+  </div>
+);
+
+export const ButtonLink = ({ children, href }) => (
+  <Center>
+    <a
+      css={css`
+        background-color: white;
+        border-radius: 5px;
+        padding: 17px 35px;
+        color: #222;
+        transition: background-color 0.2s, color 0.2s;
+        text-decoration: none;
+        font-family: "brandon-grotesque";
+        font-weight: bold;
+        font-size: 0.6em;
+        text-transform: uppercase;
+        letter-spacing: 0.05em;
+        margin-top: 50px;
+        &:hover {
+          background-color: rgb(255, 51, 71);
+          color: white;
+        }
+      `}
+      href={href}
+    >
+      {children}
+    </a>
+  </Center>
 );
